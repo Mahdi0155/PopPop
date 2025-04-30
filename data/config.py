a1c1df5eaffd256815c9f40053509936e6a1db61
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-30 12:59:31 +0330

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,26 +1,11 @@
-#===== FILE: data/config.py =====
-from aiogram.types import ChatPermissions
+#===== FOLDER: data/config.py =====
+import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
-ADMINS = [123456789, 987654321]
-CHANNEL_TAG = '@hottof | تُفِ داغ'
-CHANNEL_USERNAME = '@hottof'
+load_dotenv()
 
-FORCE_SUB_CHANNELS = [
-    "@channel1",
-    "@channel2"
-]
-
-PERMISSIONS_RESTRICTED = ChatPermissions(
-    can_send_messages=False,
-    can_send_media_messages=False,
-    can_send_other_messages=False,
-    can_add_web_page_previews=False,
-)
-
-PERMISSIONS_FULL = ChatPermissions(
-    can_send_messages=True,
-    can_send_media_messages=True,
-    can_send_other_messages=True,
-    can_add_web_page_previews=True,
-)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = list(map(int, os.getenv("ADMINS").split(',')))
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
+CHANNEL_TAG = os.getenv("CHANNEL_TAG")
+REQUIRED_CHANNELS = os.getenv("REQUIRED_CHANNELS").split(',')
