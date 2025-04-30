ae5822dad636a3fe127c6c3feaa909a6165d02d3
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-30 15:57:06 +0330

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,9 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-REQUIRED_CHANNELS = ["YourChannel1", "YourChannel2"]  # آی‌دی بدون @
-FORCE_SUB_CHANNELS = REQUIRED_CHANNELS  # اگر فرق دارن جدا تعریف کن
+ADMINS = list(map(int, os.getenv("ADMINS", "").split(',')))
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
+CHANNEL_TAG = os.getenv("CHANNEL_TAG")
+REQUIRED_CHANNELS = os.getenv("REQUIRED_CHANNELS", "").split(',')
 
-WEBHOOK_HOST = "https://<your-app-name>.onrender.com"  # آدرس دامنه Render
+# Webhook settings
+WEBHOOK_HOST = os.getenv("WEBHOOK_HOST", f"https://your-app-name.onrender.com")
 WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
