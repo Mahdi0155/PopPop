a01432becc1f2acd5f5cf5f44b185d3f2a88411f
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-30 14:10:05 +0330

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from data.config import BOT_TOKEN
 from handlers.panel import router as panel_router
-from handlers.super import router as super_router  # اضافه شده
-from handlers.post import router as post_router    # اضافه شده
-from utils.db import init_db  # مقداردهی اولیه دیتابیس
+from handlers.super import router as super_router
+from handlers.post import router as post_router
+from utils.db import init_db
 
 # ساخت ربات
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
@@ -18,8 +18,8 @@ dp = Dispatcher(storage=MemoryStorage())
 
 # اضافه کردن هندلرها
 dp.include_router(panel_router)
-dp.include_router(super_router)  # اضافه شده
-dp.include_router(post_router)   # اضافه شده
+dp.include_router(super_router)
+dp.include_router(post_router)
 
 # تعریف دستورات ربات
 async def set_commands(bot: Bot):
@@ -30,8 +30,13 @@ async def set_commands(bot: Bot):
 
 # اجرای اصلی
 async def main():
-    init_db()  # مقداردهی اولیه دیتابیس
+    init_db()
     await set_commands(bot)
+
+    # گرفتن نام کاربری ربات و ذخیره در context
+    me = await bot.get_me()
+    dp['bot_username'] = me.username
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
