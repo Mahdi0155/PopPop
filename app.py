5144144248438b4329b6e518b5245d4505ee5012
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-30 13:59:10 +0330

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from data.config import BOT_TOKEN
 from handlers.panel import router as panel_router
-from utils.db import init_db  # اضافه کردن این خط
+from handlers.super import router as super_router  # اضافه شده
+from handlers.post import router as post_router    # اضافه شده
+from utils.db import init_db  # مقداردهی اولیه دیتابیس
 
 # ساخت ربات
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
@@ -16,6 +18,8 @@ dp = Dispatcher(storage=MemoryStorage())
 
 # اضافه کردن هندلرها
 dp.include_router(panel_router)
+dp.include_router(super_router)  # اضافه شده
+dp.include_router(post_router)   # اضافه شده
 
 # تعریف دستورات ربات
 async def set_commands(bot: Bot):
